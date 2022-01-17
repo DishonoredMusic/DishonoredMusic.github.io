6c422b7bea58a724c3171c240bbb61a9a7c7d709
kvmrnn <85081978+kvmrnnn@users.noreply.github.com>
2022-01-17 22:08:35 +0300

    Delete Dishonored Music_files directory

--- Dishonored Music_files/tilda-blocks-2.7.js
+++
@@ -1,1 +0,0 @@
-function t537_setHeight(recid){var t537__el=$("#rec"+recid),t537__image=t537__el.find(".t537__bgimg:first"),t537__width=t537__image.attr("data-image-width"),t537__height=t537__image.attr("data-image-height"),t537__ratio=t537__height/t537__width,t537__padding=t537__ratio*100;$("#rec"+recid+" .t537__bgimg").css("padding-bottom",t537__padding+"%")}
